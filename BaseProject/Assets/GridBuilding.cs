11abd6b71e89cb0b6d863bc9a2785edee94c615a
Josh Fenlon <Josh2.Fenlon@live.uwe.ac.uk>
2017-11-22 14:40:00 +0000

    base

---
+++ BaseProject/Assets/GridBuilding.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBuilding : MonoBehaviour {
+
+    public Vector2 startPos;
+    public Vector2 size;
+    public Vector2 gridSectionSize;
+	public List<gridSection> grid = new List<gridSection> ();
+
+	// Use this for initialization
+	void Start () {
+		for (int a = 0; a < size.x; a++)
+		{
+			for (int b = 0; b < size.y; b++)
+			{
+				grid.Add (new gridSection (new Vector2 (a, b)));
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos()
+    {
+        for (int a = 0; a < size.x; a++)
+        {
+            for (int b = 0; b < size.y; b++)
+            {
+				Gizmos.DrawWireCube (new Vector3 (startPos.x + (a * (size.x / 2)), startPos.y + (b * (size.y / 2)), 0), new Vector3 (gridSectionSize.x, gridSectionSize.y, 1));
+            }
+        }
+    }
+}
+	
+[System.Serializable]
+public struct gridSection 
+{
+	public Vector2 xy;
+	public List<GameObject> obj;
+
+	public gridSection(Vector2 _xy) {
+		xy = _xy;
+		obj = new List<GameObject> ();
+	}
+}
--- BaseProject/Assets/Scripts/Loot Crate/Lootcrate.cs
+++ BaseProject/Assets/Scripts/Loot Crate/Lootcrate.cs
@@ -6,6 +6,7 @@ public class Lootcrate : MonoBehaviour {
 
 	public bool genCrate = false;
 	public List<float> chance = new List<float>();
+    public float maxChance = 10000;
 	public int reward = 0;
 
 	void Update() {
@@ -16,7 +17,7 @@ public class Lootcrate : MonoBehaviour {
 	}
 
 	public void generateCrate() {
-		float temp = Random.Range (0, 10000);
+		float temp = Random.Range (0, maxChance);
 		reward = -1;
 		for (int a = 0; a < chance.Count; a++) {
 			if (temp < chance[a]) {
