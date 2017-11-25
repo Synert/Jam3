ded373a7e8706ebe5d05a700c6b820331503beda
jplant20 <Josephsp@hotmail.co.uk>
2017-11-25 12:48:55 +0000

    Created the initial turret that targets and attacks an object if it hits the colliders. Will improve mechanics, such as adding damage, but wanted to push this up.

---
+++ BaseProject/Assets/Scripts/TurretAI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour
+{
+    public int curHealth;
+    public int maxHealth;
+
+    public float distance;
+    public float wakerange;
+    public float shootInterval;
+    public float bulletSpeed = 100;
+    public float bulletTimer;
+
+    public bool awake = false;
+    public bool lookingRight = true;
+
+    public GameObject bullet;
+    public Transform target;
+    public Transform shootPointLeft;
+    public Transform shootPointRight;
+
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if(target.transform.position.x > transform.position.x)
+        {
+            lookingRight = true;
+        }
+        if (target.transform.position.x < transform.position.x)
+        {
+            lookingRight = false;
+        }
+    }
+
+    public void Attack(bool attackingRight)
+    {
+        bulletTimer += Time.deltaTime;
+
+        if (bulletTimer >= shootInterval)
+        {
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
+
+            if(!attackingRight)
+            {
+                GameObject bulletClone;
+                bulletClone = Instantiate(bullet, shootPointLeft.transform.position, shootPointLeft.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+                bulletTimer = 0;
+            }
+
+            if(attackingRight)
+            {
+                GameObject bulletClone;
+                bulletClone = Instantiate(bullet, shootPointRight.transform.position, shootPointRight.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+
+                bulletTimer = 0;
+            }
+        }
+    }
+}
