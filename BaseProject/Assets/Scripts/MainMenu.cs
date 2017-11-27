8def14d787683898f25d5043e866748a0a1ca5aa
jplant20 <Josephsp@hotmail.co.uk>
2017-11-27 22:15:50 +0000

    Created a main menu and a controls menu (yet to receive final controls, so will wait on design for later) Also changed the build settings to load the scenes, added in some sound files and added some sound effects for the menus.

---
+++ BaseProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isStart;
+    public bool isControls;
+    public bool isMainMenu;
+    public bool isQuit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+            SceneManager.LoadScene("GameScene");
+        }
+        if (isControls)
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+            SceneManager.LoadScene("Controls");
+        }
+        if (isMainMenu)
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+            SceneManager.LoadScene("Main Menu");
+        }
+        if (isQuit)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            Application.Quit();
+        }
+    }
+}
