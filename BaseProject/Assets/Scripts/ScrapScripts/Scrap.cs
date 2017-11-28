e66f5be36029821e73f27943d305b5ae8841607f
StankyDan <Danthemanwow@hotmail.com>
2017-11-28 00:44:04 +0000

    Removed irrelevant scenes from project, Updated Hud visually and now accepts scrap as a child. Main scene updated with new sprites

--- BaseProject/Assets/Scripts/ScrapScripts/Scrap.cs
+++ BaseProject/Assets/Scripts/ScrapScripts/Scrap.cs
@@ -9,6 +9,7 @@ public class Scrap : MonoBehaviour
 	public bool currentlySelected = false;
 	public Vector3 difference;
 	public GameObject cursor;
+    private Rigidbody2D rigidBody;
 
     // Use this for initialization
     void Start()
@@ -37,7 +38,17 @@ public class Scrap : MonoBehaviour
 		}
     }
 
-	public void resetRigidbody() {
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Work");
+        if (col.gameObject.tag == "HUD")
+        {
+            this.transform.parent = col.transform;
+            Destroy(rigidBody);
+        }
+    }
+
+    public void resetRigidbody() {
 		GetComponent<Rigidbody2D> ().simulated = true;
 	}
 
